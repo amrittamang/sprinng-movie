5e0e73f6711da3f350ef6dbf5c386e1e8838d610
Amrit Tamang <amrit.tamang@userful.com>
2023-11-14 16:50:58 -0700

    First Commit

---
+++ src/main/java/com/movie/Movie.java
@@ -0,0 +1,30 @@
+package com.movie;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data  //generates all the boilerplate that is normally associated with simple POJOs (Plain Old Java Objects) and beans
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewIds;
+
+}
