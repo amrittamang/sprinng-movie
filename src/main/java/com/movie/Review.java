c945ce59a39f58e36d36a1efab882409859e3e25
Amrit Tamang <amrit.tamang@userful.com>
2023-11-15 10:44:54 -0700

    Added review

--- src/main/java/com/movie/Review.java
+++ src/main/java/com/movie/Review.java
@@ -8,11 +8,15 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "reviews")
-@Data
+@Data //generates all the boilerplate that is normally associated with simple POJOs (Plain Old Java Objects) and beans
 @AllArgsConstructor
 @NoArgsConstructor
 public class Review {
     @Id
     private ObjectId id;
     private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
 }
